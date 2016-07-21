8ead48ba6b1218b23d58cfaff51d72f0e4cfca4f
Magnus Eriksson <magnus.eriksson@apegroup.com>
2016-07-21 12:52:39 +0200

    Implemented retry logic with exponential backoff

---
+++ APEReactiveNetworking/Source/ExponentialSequence.swift
@@ -0,0 +1,24 @@
+//
+//  ExponentialSequence.swift
+//  APEReactiveNetworking
+//
+//  Created by Magnus Eriksson on 21/07/16.
+//  Copyright © 2016 Apegroup. All rights reserved.
+//
+
+import Foundation
+
+//Backoff strategy sequences
+
+/**
+ *  A SequenceType that creates a generator which increases exponentially
+ */
+struct ExponentialSequence: SequenceType {
+    func generate() -> AnyGenerator<NSTimeInterval> {
+        var attempt = -1
+        return AnyGenerator {
+            attempt += 1
+            return pow(2, NSTimeInterval(attempt))
+        }
+    }
+}
--- APEReactiveNetworking/Source/Network.swift
+++ APEReactiveNetworking/Source/Network.swift
@@ -13,7 +13,6 @@ import enum Result.NoError
 
 
 
-
 public enum NetworkError : ErrorType {
     case ParseFailure
     case MissingData
@@ -23,14 +22,15 @@ public enum NetworkError : ErrorType {
     case TimedOut
 }
 
-
 // TODO: Consider customisable data, download, upload tasks
 
 public struct Network {
     
     /// The TOTAL number of seconds to wait before aborting the entire operation.
     static let operationTimeoutSeconds : NSTimeInterval = 20
-    private static let retryCount = 10
+    
+    /// The max number of retries before aborting the entire operation
+    static let retryCount = 10
     
     
     //MARK: Public
@@ -38,14 +38,24 @@ public struct Network {
     public init() {}
     
     /**
-     Sends a request over the network
+     Sends a request over the network. 
+     
+     Aborts and fails the operation after 'abortAfter' seconds.
      
      - parameter request:               The request to be sent
-     - parameter responseCodeValidator: An http status code validator that asserts that the received response code matches the expected response code. Defaults to 'ApeResponseCodeValidator'
+     
+     - parameter responseCodeValidator: An http status code validator that asserts that the received response code matches the expected response code.
+     Defaults to 'ApeResponseCodeValidator'
+     
      - parameter session:               The NSURLSession to be used. Defaults to the shared session
-     - parameter scheduler:             The scheduler to which the returned SignalProducer will forward events to
-     - parameter abortAfter:            Number of seconds to wait until the operation is aborted and a 'TimedOut' failure is sent
-     - parameter parseDataBlock:   If response data is expected you should provide this parameter as a means to parse the data to the expected data type.
+     
+     - parameter scheduler:             The scheduler to which the returned SignalProducer will forward events to. Defaults to the UIScheduler.
+     
+     - parameter abortAfter:            Number of seconds to wait until the operation is aborted and a 'TimedOut' failure is sent. Defaults to 'operationTimeoutSeconds'
+     
+     - parameter maxRetries:            Max number of retries before failing the operation. Implements an exponential backoff between each retry. Defaults to 'retryCount'
+     
+     - parameter parseDataBlock:        If response data is expected you should provide this parameter as a means to parse the data to the expected data type.
      
      - returns: A SignalProducer that will begin the network request when started
      */
@@ -54,14 +64,16 @@ public struct Network {
                      session: NSURLSession = NSURLSession.sharedSession(),
                      scheduler: SchedulerType = UIScheduler(),
                      abortAfter: NSTimeInterval = operationTimeoutSeconds,
+                     maxRetries: Int = retryCount,
                      parseDataBlock: ((data:NSData) -> T?)? = nil) -> SignalProducer<T, NetworkError> {
         
         
-        /// Create a SignalProducer that will fail after 'timeout' seconds
+        /// Create a SignalProducer that will fail after 'abortAfter' seconds
         var timeoutObserver: Observer<T, NetworkError>?
         let timeoutProducer = SignalProducer<T, NetworkError> { observer, _disposable in
             timeoutObserver = observer
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(abortAfter * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(abortAfter * Double(NSEC_PER_SEC))),
+            dispatch_get_main_queue()) {
                 timeoutObserver?.sendFailed(NetworkError.TimedOut)
             }
         }
@@ -71,21 +83,37 @@ public struct Network {
             responseCodeValidator: responseCodeValidator,
             parseDataBlock: parseDataBlock)
             .injectNetworkActivityIndicatorSideEffect()  //NOTE: injection must always be done before other RAC operations since it will create a new SignalProducer
-            .retry(Network.retryCount) //FIXME: add a delay between each retry
+            .retryWithExponentialBackoff(maxRetries)
             .on(completed: {
                 //Complete and invalidate the Timeout-SignalProducer when the operation is completed, in order to complete the merged SignalProducer below
                 timeoutObserver?.sendCompleted()
                 timeoutObserver = nil
-                })
+            })
         
         
-        /// Merge the Timeout-SignalProducer and the Operation-SignalProducer
-        let mergedProducer: SignalProducer<T, NetworkError> =
-            SignalProducer<SignalProducer<T, NetworkError>, NoError> (values: [timeoutProducer, operationProducer])
-                .flatten(.Merge)
-                .observeOn(scheduler)
-        
-        return mergedProducer
+        /* Merge the Timeout-SignalProducer and the Operation-SignalProducer.
+         * The Merged-SignalProducer will:
+         *      - fail if one of the inner SignalProducers fail,
+         *      or
+         *      - complete if both inner SignalProducers complete
+         *
+         * Whichever occurs first.
+         */
+        return SignalProducer<SignalProducer<T, NetworkError>, NoError> (values: [timeoutProducer, operationProducer])
+            .flatten(.Merge)
+            .observeOn(scheduler)
+            .on (
+                started: {
+                    print("# NetworkOperationSignal started")
+                }, failed: { error in
+                    print("# NetworkOperationSignal failed: \(error)")
+                }, completed: {
+                    print("# NetworkOperationSignal completed")
+                }, interrupted: {
+                    print("# NetworkOperationSignal interrupted")
+                }, terminated: {
+                    print("# NetworkOperationSignal terminated")
+            })
     }
 }
 
@@ -157,19 +185,51 @@ private extension SignalProducer {
      - returns: The signal producer
      */
     func injectNetworkActivityIndicatorSideEffect() -> SignalProducer<Value, Error> {
-        return self.on ( started: {
-            print("# signal started")
-            NetworkActivityIndicator.sharedInstance.enabled = true
-            }, failed: { error in
-                print("# signal failed: \(error)")
-            }, completed: {
-                print("# signal completed")
-            }, interrupted: {
-                print("# signal interrupted")
+        return self.on (
+            started: {
+                NetworkActivityIndicator.sharedInstance.enabled = true
             }, terminated: {
-                print("# signal terminated")
                 NetworkActivityIndicator.sharedInstance.enabled = false
             }
         )
+    }
+}
+
+//MARK: SignalProducerType + Retry Exponential Backoff
+
+private extension SignalProducerType {
+    
+     ///Retries the SignalProducer 'maxAttempts' number of times before failing. Implements an exponential backoff between each retry.
+    func retryWithExponentialBackoff(maxAttempts: Int)
+        -> SignalProducer<Value, Error> {
+            return retryWithBackoff(ExponentialSequence(), attemptsLeft: maxAttempts)
+    }
+    
+    
+    private func retryWithBackoff<S: SequenceType where S.Generator.Element == NSTimeInterval>(strategy: S, attemptsLeft: Int)
+        -> SignalProducer<Value, Error> {
+            
+            guard attemptsLeft > 0 else {
+                print("No attempts left. Aborting.")
+                return self.producer
+            }
+            
+            var generator = strategy.generate()
+            guard let timeout = generator.next() else {
+                print("Failed to generate next timeout. Aborting.")
+                return self.producer
+            }
+            
+            // If an error occurs we catch it and map it to a new concatenated SignalProducer which will:
+            //  - Create an inner SignalProducer that will wait 'timout' seconds
+            //  - Replay the original SignalProducer when the previously delayed signal has completed
+            return self.flatMapError { _ -> SignalProducer<Value, Error> in
+                print("\(NSDate()): Received an error. Timing out for: \(timeout) seconds")
+                return SignalProducer.empty
+                    .delay(timeout, onScheduler: QueueScheduler())
+                    .concat(self.retryWithBackoff(GeneratorSequence(generator), attemptsLeft: attemptsLeft-1))
+                }.on (started: {
+                    print("\(NSDate()): Starting operation. Attempts left: \(attemptsLeft)")
+                })
     }
 }
